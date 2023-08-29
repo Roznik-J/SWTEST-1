7de8115acdae6939d99db6ce69aa5b8e93957415
Roznik-J <roznikj@myumanitoba.ca>
2023-08-29 02:17:18 -0400

    Problem 1.3 Finished

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mvn test -Dtest="SLListAddTester"
+#mvn test -Dtest="SLListAddTester"
+mvn test -Dtest="SLListRepOkTester"
--- src/test/java/SLListRepOkTester.java
+++ src/test/java/SLListRepOkTester.java
@@ -9,12 +9,69 @@ public class SLListRepOkTester {
         SLList l = new SLList();
         assertTrue(l.repOk());
     }
-
+    
+    // Test 1: 1 Node, ACyclic
     @Test public void t1() {
         SLList l = new SLList();
         Node n = new Node();
         // your code goes here
+	n.elem = true;
+	l.header = n;
 
+	assertTrue(l.repOk());
+    }
+   
+    // Test 2: 1 Node, Cyclic
+    @Test public void t2() {
+        SLList l = new SLList();
+        Node n = new Node();
+        // your code goes here
+        n.elem = true;
+	n.next = n;
+	l.header = n;
+
+        assertFalse(l.repOk());
+    }
+    // Test 3: 2 Nodes, ACyclic
+    @Test public void t3() {
+        SLList l = new SLList();
+        Node n = new Node();
+        // your code goes here
+        n.elem = true;
+	l.header = n;
+	Node m = new Node();
+	m.elem = true;
+	n.next = m;
+
+        assertTrue(l.repOk());
+    }
+    // Test 4: 2 Nodes, Cyclic
+    @Test public void t4() {
+        SLList l = new SLList();
+        Node n = new Node();
+        // your code goes here
+        n.elem = true;
+	l.header = n;
+        Node m = new Node();
+        m.elem = true;
+        n.next = m;
+	m.next = m;
+
+        assertFalse(l.repOk());
+    }
+    // Test 5: 2 Nodes, Cyclic
+    @Test public void t5() {
+        SLList l = new SLList();
+        Node n = new Node();
+        // your code goes here
+        n.elem = true;
+	l.header = n;
+        Node m = new Node();
+        m.elem = true;
+        n.next = m;
+        m.next = n;
+
+        assertFalse(l.repOk());
     }
 
     // your code goes here
